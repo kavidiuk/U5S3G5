f70dcb727923ac5750f79fbdcb2976b0d6d65cc4
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2024-02-23 16:14:35 +0100

    add OrganizzatoreDiEventi

---
+++ src/main/java/it/epicode/be/U5S3G5/OrganizzatoreDiEventi.java
@@ -0,0 +1,9 @@
+package it.epicode.be.U5S3G5;
+
+public class OrganizzatoreDiEventi {
+    public String titolo;
+    public String descrizione;
+    public Integer data;
+    public String luogo;
+    public Integer numeroDiPosti;
+}
---
+++ src/main/java/it/epicode/be/U5S3G5/UtenteNormale.java
@@ -0,0 +1,4 @@
+package it.epicode.be.U5S3G5;
+
+public class UtenteNormale {
+}
